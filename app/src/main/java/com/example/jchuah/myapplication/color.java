9f80ed15e56198ac1c2d868a0e71542d87413ce6
benji18808 <benjijosh57@gmail.com>
2015-10-20 12:36:06 -0500

    made button go on to next screen

--- app/src/main/java/com/example/jchuah/myapplication/color.java
+++ app/src/main/java/com/example/jchuah/myapplication/color.java
@@ -1,37 +1,23 @@
 package com.example.jchuah.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class color extends AppCompatActivity {
 
-    Bundle sendBundle = new Bundle();
-    static String tag = "com.example.jchuah.myapplication.MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_color);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_color, menu);
         return true;
-    }
-
-    public void onNextClick(View source){
-        //Toast.makeText(this, "Howdy Honda",Toast.LENGTH_LONG).show();
-        Log.i(tag , "next button click");
-        Intent colorActivityIntent = new Intent(this, color.class);
-        startActivity(colorActivityIntent, sendBundle);
-
     }
 
     @Override
