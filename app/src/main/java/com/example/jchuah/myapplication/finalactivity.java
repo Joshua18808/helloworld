6a44923af6dfe8b2689ed25f427e2fdb605bf124
benji18808 <benjijosh57@gmail.com>
2015-10-28 12:18:29 -0500

    buttons now toast howdy honda

--- app/src/main/java/com/example/jchuah/myapplication/finalactivity.java
+++ app/src/main/java/com/example/jchuah/myapplication/finalactivity.java
@@ -2,31 +2,22 @@ package com.example.jchuah.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class color extends AppCompatActivity {
-    Bundle groceries;
+public class finalactivity extends AppCompatActivity {
 
-    public void onColorClick (View Button){
-        Toast.makeText(this, "Howdy Honda", Toast.LENGTH_LONG).show();
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_color);
-        groceries = getIntent().getBundleExtra("groceries");
-        Log.i("GROCERIES!", groceries.getString("name"));
+        setContentView(R.layout.activity_final);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_color, menu);
+        getMenuInflater().inflate(R.menu.menu_finalactivity, menu);
         return true;
     }
 
